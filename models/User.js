c9d4343d6524f75a93cc94af91c43b03476db0a2
spriveen <rpeanuka@gmail.com>
2025-05-06 21:40:24 -0700

    #

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema(
 );
 
 
-const User= mongoose.models.User || mongoose.models("User",
+const User= mongoose.models.User || mongoose.model("User",
  UserSchema)
 
  export default User;
